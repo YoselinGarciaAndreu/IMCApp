b4746d57535947d121358ffc80d40cf331ef0656
Jose <mail@mail.org>
2023-11-15 12:30:49 +0100

    version antes de comenzar otra vez

--- app/src/main/java/com/pmdm/imcapp/ImcCalculatorActivity.kt
+++ app/src/main/java/com/pmdm/imcapp/ImcCalculatorActivity.kt
@@ -3,26 +3,63 @@ package com.pmdm.imcapp
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.cardview.widget.CardView
+import androidx.core.content.ContextCompat
+import java.lang.reflect.RecordComponent
 
 
 class IMCCalculatorActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        initListeners()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_layout_imc_calculator2)
+        initComponents()
+        initListeners()
+        initUI()
+
     }
 
     private lateinit var viewMale: CardView
     private lateinit var viewFemale: CardView
-    private var isMale: Boolean = true
+    private var isMaleSelected: Boolean = true
 
     private fun initComponents() {
         viewMale = findViewById(R.id.viewMale)
         viewFemale = findViewById(R.id.viewFemale)
     }
 
+
+
     private fun initListeners(){
-        viewMale.setOnClickListener()
+        viewMale.setOnClickListener(){
+            setGenderColor()
+            isMaleSelected = true}
+
+        viewFemale.setOnClickListener(){
+            setGenderColor()
+            isMaleSelected = false}
+        }
+
+    private fun setGenderColor(){
+        viewMale.setCardBackgroundColor(getBackgroundColor(isMaleSelected))
+        viewFemale.setCardBackgroundColor(getBackgroundColor(!isMaleSelected))
+    }
+
+    private fun getBackgroundColor(isComponentSelected : Boolean): Int{
+        val colorReference = if(isComponentSelected) {
+            R.color.bg_comp_sel
+        } else {
+            R.color.bg_comp
+        }
+        return ContextCompat.getColor(this,colorReference)
+    }
+
+    private fun initUI(){
+        setGenderColor()
+    }
+
+
+    private fun isMaleSelected(){
+
     }
 
 }
+
